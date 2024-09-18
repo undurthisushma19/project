2d9802b8d5b80bd56fd361123c302c4cc4eff90b
UNDURTHISUSHMA <usushma19@gmail.com>
2024-09-18 15:06:47 +0530

    Add files via upload

---
+++ ScholarsBoard.Communication/Email/Message.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScholarsBoard.Communication.Email
+{
+    public class Message
+    {
+        public List<MailboxAddress> To { get; set; }
+        public string? Subject { get; set; }
+        public string? Content { get; set; }
+
+        public IFormFileCollection? Attachments { get; set; }
+
+        public Message(IEnumerable<string> to, string subject, string content, IFormFileCollection attachments)
+        {
+            To = new List<MailboxAddress>();
+
+            foreach (var item in to)
+            {
+                To.Add(MailboxAddress.Parse(item));
+            }
+            Subject = subject;
+            Content = content;
+            Attachments = attachments;
+        }
+    }
+}
